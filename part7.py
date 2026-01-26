27216694cc2df5b9f72de44e02322e49fd091250
Taj Tucker <tucker8258@gmail.com>
2026-01-25 19:58:09 -0600

    Finish Part 7 of Coding Assignment

---
+++ part7.py
@@ -0,0 +1,9 @@
+#Create a compound data structure for the elements in the periodic table. You should include only any five elements from the periodic table. For example, you can use the following five elements. Use symbol as the key.
+
+periodic_dictionary = {"C": {"Name": "Carbon", "Atomic Number": "6", "Atomic Mass": "12"},
+                       "O": {"Name": "Oxygen", "Atomic Number": "8", "Atomic Mass": "16"},
+                       "H": {"Name": "Hydrogen", "Atomic Number": "1", "Atomic Mass": "1"},
+                       "He": {"Name": "Helium", "Atomic Number": "2", "Atomic Mass": "4"},
+                       "N": {"Name": "Nitrogen", "Atomic Number": "7", "Atomic Mass": "14"}}
+
+print(periodic_dictionary)
