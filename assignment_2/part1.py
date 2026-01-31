1d3ef3727e2e6ccfe7ebcd8b70693a60a1141b78
Taj Tucker <tucker8258@gmail.com>
2026-01-31 17:35:52 -0600

    Create 4 Parts for Assignment 2 and Add Prompt for Part1

---
+++ assignment_2/part1.py
@@ -0,0 +1,4 @@
+# Part-1 (20 Points): Write a program that asks the user to enter information about a group of students using a while loop. The information includes name (data type String) and score (data type float). The program displays the name with the highest score. The number of students is given by the user.
+
+# The program creates two empty lists (one for names and one for scores) and fills them in the while loop. Then, you can use index method of the list and max function to find the index of the maximum score.
+
