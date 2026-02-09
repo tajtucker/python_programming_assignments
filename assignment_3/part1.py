035c1d5e05f8dab260bab48d01aff2f5923fb69e
Taj Tucker <tucker8258@gmail.com>
2026-02-08 21:55:38 -0600

    Finish Part 1 of Assignment 3

---
+++ assignment_3/part1.py
@@ -0,0 +1,9 @@
+# Write a function called factorial that takes an integer and returns an integer. It calculates factorial of the input argument. For example, factorial(5) returns 120 and factorial(6) returns 720. 
+
+def factorial(n):
+    result = 1
+    for i in range(1, n+1):
+        result *= i
+    return result
+
+print(factorial(5))
