a36ed3800f0d1f729d09542f8c444b71a5b8ad97
Taj Tucker <tucker8258@gmail.com>
2026-02-22 18:58:20 -0600

    Small File Changes and Create part1 for Assignment 5

--- assignment_3/part2.py
+++ assignment_3/part2.py
@@ -1,7 +1,7 @@
 # Compute 𝑒x: You already developed your factorial function in Part-1. You are required to use that function. Let the user input the value of 𝑥. Continue adding terms until the difference of successive terms is less than 0.001. Compare your answer with the Python "math.e ** x". You should show your results for 𝑥=1, 𝑥=2 and 𝑥=3.
 
 import math
-from part1 import factorial
+from assignment_3.part1 import factorial
 
 def computeE(x):
     e = 1
