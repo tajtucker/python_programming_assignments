2c8796e20e1138c1d8db199a81d2d12784a78504
Taj Tucker <tucker8258@gmail.com>
2026-03-15 20:50:26 -0500

    Finish Part 1 of Assignment 6

--- assignment_6/part1.py
+++ assignment_6/part1.py
@@ -1,2 +1,16 @@
 # Write a program that reads a sequence of integers into a NumPy array and that computes the alternating sum of all elements in the array. For example, if the program is executed with the input data 
 # 1 4 9 16 9 7 4 9 11 then it computes 1-4+9-16+9-7+4-9+11 = -2
+
+import numpy as np
+
+arr = np.array([1, 4, 9, 16, 9, 7, 4, 9, 11])
+
+x = 0
+
+for i in range(len(arr)):
+    if i % 2 == 0:
+        x += arr[i]
+    else:
+        x -= arr[i]
+
+print(x)
